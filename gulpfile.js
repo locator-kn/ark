07a4b33fe8caf5041078ef78e698ace89516623c
Timo Weiß <info@timo-weiss.com>
2015-03-26 22:35:19 +0100

    add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var jshint = require('gulp-jshint');
+
+
+gulp.task('jshint', function () {
+      return gulp.src('./lib/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
+});
 
 
 gulp.task('develop', function () {
-  nodemon({ script: 'index.js' })
+  nodemon({
+    script: 'index.js',
+    tasks: ['jshint']
+    })
     .on('restart', function () {
       console.log('restarted!');
     });
