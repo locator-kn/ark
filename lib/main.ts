feefb24e0e40ecc7d268b1bcefebd6d71cbd81d9
Timo Weiß <info@timo-weiss.com>
2015-03-31 17:20:38 +0200

    add user plugin
    Conflicts:
    	lib/main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,16 @@
 /// <reference path="../typings/hapi/hapi.d.ts" />
 var Hapi = require('hapi');
+var DatabaseService = require('./database/databaseService');
+
+var dbInstance = new DatabaseService.DatabaseService();
+
+var UserPlugin = require('./plugins/user/userPlugin').UserPlugin;
+var userPlugin = new UserPlugin();
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+
+server.connection({ port: 3001 });
+
 
 server.route({
     method: 'GET',
@@ -11,6 +19,14 @@ server.route({
         reply('Hello, emily!');
     }
 });
+
+server.register({
+    register: userPlugin,
+    options: {
+        database: 'http://localhost:5984',
+        databaseInstance: dbInstance.db
+    }
+}, userPlugin.errorInit);
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
