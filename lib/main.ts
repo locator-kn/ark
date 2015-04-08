371f1abb86035ff3452998f13705c025ac0745ca
Timo Weiß <info@timo-weiss.com>
2015-04-08 14:31:10 +0200

    add sample usage of plugins, add prefix for routes

--- lib/main.ts
+++ lib/main.ts
@@ -1,31 +1,28 @@
 /// <reference path="../typings/hapi/hapi.d.ts" />
 var Hapi = require('hapi');
-var DatabaseService = require('./database/databaseService');
+var DatabasePlugin = require('bemily-database');
+var UserPlugin = require('bemily-user');
 
-var dbInstance = new DatabaseService.DatabaseService();
-
-var UserPlugin = require('./plugins/user/userPlugin').UserPlugin;
+var databasePlugin = new DatabasePlugin();
 var userPlugin = new UserPlugin();
+
+var routeOption = {
+    routes: {
+        prefix: '/api/v1'
+    }
+};
 
 var server = new Hapi.Server();
 
-server.connection({ port: 3001 });
-
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, Bemily!');
-    }
-});
+server.connection({port: 3001});
 
 server.register({
-    register: userPlugin,
-    options: {
-        databaseInstance: dbInstance.db
-    }
-}, userPlugin.errorInit);
+    register: userPlugin
+}, routeOption, userPlugin.errorInit);
+
+server.register({
+    register: databasePlugin
+}, routeOption, databasePlugin.errorInit);
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
