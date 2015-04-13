8c6a3a13d6e8d49c4b698becd9d58193f5cac66b
Timo Weiß <info@timo-weiss.com>
2015-04-13 16:08:57 +0200

    add auth

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,16 @@
 /// <reference path="../typings/hapi/hapi.d.ts" />
 var Hapi = require('hapi');
+
+// Plugins
 var DatabasePlugin = require('bemily-database');
 var UserPlugin = require('bemily-user');
 
-var databasePlugin = new DatabasePlugin();
+// Thirdparty plugins
+var AuthPlugin = require('bemily-authentication');
+
+var auth = new AuthPlugin();
+
+var databasePlugin = new DatabasePlugin('app', 'http://emily.iriscouch.com', 80);
 var userPlugin = new UserPlugin();
 
 var routeOption = {
@@ -15,6 +22,19 @@ var routeOption = {
 var server = new Hapi.Server();
 
 server.connection({port: 3001});
+
+server.register(auth, routeOption, function (err) {
+    console.log(err, 'bla');
+});
+server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+        handler: function (request, reply) {
+            reply('hello, ' + 'kjhasd');
+        }
+    }
+});
 
 server.register({
     register: userPlugin
