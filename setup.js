d92ca396eb9e73ea3f31ff4c467645ce8ac59270
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-14 10:41:21 +0200

    little refactoring and better logging

--- setup.js
+++ setup.js
@@ -7,32 +7,37 @@ var uri = 'http://locator.in.htwg-konstanz.de';
 
 // Database plugin
 // TODO: save params in env.json
-var db = new Database('app', envVariables.db.production, uri, 5984);
+var db = new Database('setup', envVariables.db.production, uri, 5984);
 var data = new StaticData();
 var trip = new Trip();
 
-console.log('Setting up Database and all needed Views and Data.');
-
+console.log('Setting up Database and all needed Views and Data:');
 
 db.setup(function (err) {
 
     if (err) {
-        console.error('Setting up database in Database-Plugin failed')
+        console.error('Setting up database in Database-Plugin failed', err);
+        return;
     }
+    console.log('Database setup successful');
 });
-
-return;
 
 trip.setup(db, function (err) {
 
     if (err) {
-        console.error('Setting up search algo in Trip-Plugin failed')
+        console.error('Setting up search algo in Trip-Plugin failed', err);
+        return;
     }
+    console.log('Search-Trip setup successful')
 });
 
+return;
 data.setup(db, function (err) {
 
     if (err) {
-        console.error('Setting up all static data in Static-Data-Plugin failed')
+        console.error('Setting up all static data in Static-Data-Plugin failed', err);
+        return;
     }
 });
+
+
