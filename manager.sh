abde0f8f38bf052faeb9329250edbfbff7930425
Timo Weiß <info@timo-weiss.com>
2015-07-08 20:24:22 +0200

    use forever to start process

--- manager.sh
+++ manager.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "starting nodes"
-node index.js PORT 3001 RPORT 3002 &
-node index.js PORT 3003 RPORT 3004
+forever start index.js PORT 3001 RPORT 3002 &
+forever start index.js PORT 3003 RPORT 3004
 
